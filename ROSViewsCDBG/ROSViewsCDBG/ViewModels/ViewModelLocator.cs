96dfceb7e3ac2ea7845d96f06c29532413ab9bc0
sebcul <cullbrand@gmail.com>
2017-04-20 09:40:15 +0200

    Added viewmodellocator.

---
+++ ROSViewsCDBG/ROSViewsCDBG/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ROSViewsCDBG.Views.UserControls;
+
+namespace ROSViewsCDBG.ViewModels
+{
+    public class ViewModelLocator
+    {
+        public static ClubAdminView ClubAdminView
+        {
+            get { return new ClubAdminView(); }
+        }
+    }
+}
