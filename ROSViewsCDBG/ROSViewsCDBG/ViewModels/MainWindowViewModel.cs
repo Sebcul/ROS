523bab30777d4868626697f7a0372b867d5b90e0
Sebastian Cullbrand <cullbrand@gmail.com>
2017-04-20 22:22:42 +0200

    Added command for edit user.

--- ROSViewsCDBG/ROSViewsCDBG/ViewModels/MainWindowViewModel.cs
+++ ROSViewsCDBG/ROSViewsCDBG/ViewModels/MainWindowViewModel.cs
@@ -3,20 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using ROSViewsCDBG.Helper_classes;
+using ROSViewsCDBG.Views.UserControls;
 
 namespace ROSViewsCDBG.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-
+        private ICommand _editUserInfoCommand;
+        private object _selectedUserControl;
+        private string _email;
 
         public MainWindowViewModel()
         {
             RegisterMessages();
         }
 
-        private string _email;
+        public ICommand EditUserInfoCommand
+        {
+            get { return _editUserInfoCommand ?? (_editUserInfoCommand = new RelayCommand(OpenEditUserInfo)); }
+        }
+
+        public object SelectedUserControl
+        {
+            get { return _selectedUserControl; }
+            set
+            {
+                _selectedUserControl = value;
+                OnPropertyChanged();
+            }
+        }
 
         public string Email
         {
@@ -26,6 +43,13 @@ namespace ROSViewsCDBG.ViewModels
                 _email = value;
                 OnPropertyChanged();
             }
+        }
+
+        private void OpenEditUserInfo(object obj)
+        {
+            var editUserInfoWindow = new UserInfoView();
+            SelectedUserControl = editUserInfoWindow;
+
         }
 
         private void RegisterMessages()
