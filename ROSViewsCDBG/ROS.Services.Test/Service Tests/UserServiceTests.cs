5f210ed1bfa2f75f068e12f4c046eae6c527674c
Kusken <jallowski@hotmail.se>
2017-04-20 22:08:38 +0200

    Added method FindUserByEmail to the service!

--- ROSViewsCDBG/ROS.Services.Test/Service Tests/UserServiceTests.cs
+++ ROSViewsCDBG/ROS.Services.Test/Service Tests/UserServiceTests.cs
@@ -9,20 +9,22 @@ namespace ROS.Services.Test.Service_Tests
 {
     public class UserServiceTests
     {
+
+        private List<User> _testData = new List<User>
+        {
+             new User {Id = 1, Active = true, FirstName = "John", LastName = "Doe", Email = "doedoe_@gmail.com"},
+             new User {Id = 2, Active = true, FirstName = "Eric", LastName = "Evans", Email = "evan_s@hotmail.com"},
+             new User {Id = 3, Active = false, FirstName = "Martin", LastName = "Fowler", Email = "m_fowler@greatest.com"},
+             new User {Id = 4, Active = true, FirstName = "Kent", LastName = "Beck", Email = "commisarie_beck@snuten.se"},
+             new User {Id = 5, Active = true, FirstName = "Uncle", LastName = "Bob", Email = "uncle_b0b@grottmail.com"}
+        };
+
+
         [Fact]
         public void Should_ReturnAllActiveEntities_When_GetAllActiveEntitiesIsCalled()
         {
             //Arrange
-            var data = new List<User>
-            {
-                new User {Id = 1, Active = true, FirstName = "John", LastName = "Doe"},
-                new User {Id = 2, Active = true, FirstName = "Eric", LastName = "Evans"},
-                new User {Id = 3, Active = false, FirstName = "Martin", LastName = "Fowler"},
-                new User {Id = 4, Active = true, FirstName = "Kent", LastName = "Beck"},
-                new User {Id = 5, Active = true, FirstName = "Uncle", LastName = "Bob"}
-            };
-
-            var stubRepositoryFactory = A.RepositoryFactory().ThatReturnsAFakeRepositoryWithTestUsers(data).Build();
+            var stubRepositoryFactory = A.RepositoryFactory().ThatReturnsAFakeRepositoryWithTestUsers(_testData).Build();
 
             var userServiceSut = new UserService(stubRepositoryFactory);
 
@@ -34,6 +36,25 @@ namespace ROS.Services.Test.Service_Tests
             //Assert
             Assert.Equal(true, containsAllActiveEntities);
 
+        }
+
+
+        [Theory]
+        [InlineData("doedoe_@gmail.com")]
+        [InlineData("uncle_b0b@grottmail.com")]
+        public void Should_ReturnUser_When_EmailIsMatchingAndUserIsActive(string expectedEmail)
+        {
+            //Arrange
+            var stubRepositoryFactory = A.RepositoryFactory().ThatReturnsAFakeRepositoryWithTestUsers(_testData).Build();
+
+            var userServiceSut = new UserService(stubRepositoryFactory);
+
+            //Act
+            var resultUser = userServiceSut.FindUserByEmail(expectedEmail);
+
+
+            //Assert
+            Assert.Equal(expectedEmail, resultUser.Email);
         }
     }
 
@@ -52,6 +73,12 @@ namespace ROS.Services.Test.Service_Tests
         public IEnumerable<User> GetAllUsers()
         {
             return _repository.GetAllWhereEntitiesMatchPredicate(user => user.Active);
+        }
+
+
+        public User FindUserByEmail(string email)
+        {
+            return _repository.GetAllWhereEntitiesMatchPredicate(user => user.Email == email && user.Active).First();
         }
     }
 
