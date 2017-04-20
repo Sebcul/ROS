d1a79a3ac5772fd6b22509bb67c6b7fd8eec2609
sebcul <cullbrand@gmail.com>
2017-04-20 10:31:06 +0200

    Added static resource to vmlocator on some views. Created some viewmodels.

--- ROSViewsCDBG/ROSViewsCDBG/ViewModels/ViewModelLocator.cs
+++ ROSViewsCDBG/ROSViewsCDBG/ViewModels/ViewModelLocator.cs
@@ -9,9 +9,75 @@ namespace ROSViewsCDBG.ViewModels
 {
     public class ViewModelLocator
     {
-        public static ClubAdminView ClubAdminView
+        public static ClubAdminViewModel ClubAdminViewModel
         {
-            get { return new ClubAdminView(); }
+            get { return new ClubAdminViewModel(); }
         }
+
+        public static ClubInfoViewModel ClubInfoViewModel
+        {
+            get { return new ClubInfoViewModel(); }
+        }
+
+        public static ClubMemberViewModel ClubMemberView
+        {
+            get { return new ClubMemberViewModel(); }
+        }
+
+        public static ClubRegistrationViewModel ClubRegistrationViewModel
+        {
+            get { return new ClubRegistrationViewModel(); }
+        }
+
+        public static CreateClubViewModel CreateClubViewModel
+        {
+            get { return new CreateClubViewModel(); }
+        }
+
+        public static CreateEntryViewModel CreateEntryViewModel
+        {
+            get { return new CreateEntryViewModel(); }
+        }
+
+        public static CreateRaceEventViewModel CreateRaceEventViewModel
+        {
+            get { return new CreateRaceEventViewModel(); }
+        }
+
+        public static CreateRegattaViewModel CreateRegattaViewModel
+        {
+            get { return new CreateRegattaViewModel(); }
+        }
+
+        public static CreateSocialEventViewModel CreateSocialEventViewModel
+        {
+            get { return new CreateSocialEventViewModel(); }
+        }
+
+        public static DistanceMeasuredRaceEventInfoViewModel DistanceMeasuredRaceEventInfoViewModel
+        {
+            get { return new DistanceMeasuredRaceEventInfoViewModel(); }
+        }
+
+        public static EditUserInfoViewModel EditUserInfoViewModel
+        {
+            get { return new EditUserInfoViewModel(); }
+        }
+
+        public static EntryInfoViewModel EntryInfoViewModel
+        {
+            get { return new EntryInfoViewModel(); }
+        }
+
+        public static ListUsersClubsViewModel ListUsersClubViewModel
+        {
+            get { return new ListUsersClubsViewModel(); }
+        }
+
+        public static ListUsersRegattasViewModel ListUsersRegattasViewModel
+        {
+            get { return new ListUsersRegattasViewModel(); }
+        }
+        
     }
 }
