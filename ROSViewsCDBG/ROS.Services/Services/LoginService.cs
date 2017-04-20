61412f55bbdd6b1387b9fd9ff181bace66ab0386
danieljaller <danieljaller@gmail.com>
2017-04-20 10:32:25 +0200

    created login service. Not ready.

---
+++ ROSViewsCDBG/ROS.Services/Services/LoginService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
+using System.Linq;
+using ROSPersistence.Repository;
+using ROSPersistence.ROSDB;
+
+namespace ROS.Services.Services
+{
+    public class LoginService
+    {
+        private IRepository<User> repo;
+
+        public LoginService()
+        {
+            repo = RepositoryFactory.Instance.CreateRepository<User>();
+        }
+
+        public bool ConfirmUserCredentials(string email, string password)
+        {
+            SqlParameter param1 = new SqlParameter("@Email", email);
+            SqlParameter param2 = new SqlParameter("@Password", password);
+            int result = 0;
+            SqlParameter param3 = new SqlParameter("@responseMessage", result);
+            var context = new ROSDB();
+            int x = context.Database.SqlQuery<int>("EXEC sp_ApplicationUserLogin @Email, @Password, @responseMessage", param1, param2, param3).First();
+            return Convert.ToBoolean(x);
+            
+        }
+    }
+}
