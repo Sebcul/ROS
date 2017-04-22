2e3fef6dad6c8247e4a916c60ae9847a5e7184cd
danieljaller <danieljaller@gmail.com>
2017-04-22 16:44:29 +0200

    Started working on CreateRegattaView and CreateRegattaViewModel. Now life's calling.

--- ROSViewsCDBG/ROSViewsCDBG/ViewModels/CreateRegattaViewModel.cs
+++ ROSViewsCDBG/ROSViewsCDBG/ViewModels/CreateRegattaViewModel.cs
@@ -1,12 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ROSPersistence.ROSDB;
 
 namespace ROSViewsCDBG.ViewModels
 {
     public class CreateRegattaViewModel : ViewModelBase
     {
+        private string _name;
+        private ObservableCollection<IUser> _admins;
+        private string _location;
+        private DateTime _starTime;
+        private DateTime _endTime;
+        private string _description;
+        private ObservableCollection<Event> _events;
+        private ObservableCollection<IClub> _clubs;
+
+        public CreateRegattaViewModel()
+        {
+            
+        }
+
+        public string Name
+        {
+            get => _name;
+            set { _name = value; OnPropertyChanged();}
+        }
+
+        public ObservableCollection<IUser> Admins
+        {
+            get => _admins;
+            set { _admins = value; OnPropertyChanged();} 
+        }
+
+        public string Location
+        {
+            get => _location;
+            set { _location = value; OnPropertyChanged();}
+        }
+
+        public DateTime StarTime
+        {
+            get => _starTime;
+            set { _starTime = value; OnPropertyChanged();}
+        }
+
+        public DateTime EndTime
+        {
+            get => _endTime;
+            set { _endTime = value; OnPropertyChanged();}
+        }
+
+        public string Description
+        {
+            get => _description;
+            set { _description = value; OnPropertyChanged();}
+        }
+
+        public ObservableCollection<Event> Events
+        {
+            get => _events;
+            set { _events = value; OnPropertyChanged();}
+        }
+
+        
+        public ObservableCollection<IClub> Clubs
+        {
+            get => _clubs;
+            set { _clubs = value; OnPropertyChanged();}
+        }
+
     }
 }
