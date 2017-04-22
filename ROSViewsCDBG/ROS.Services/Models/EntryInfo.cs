4b3fde32938ae6cc1b4a9ec7f8337354bd22ad44
Samuel <Sam.Davidsson86@gmail.com>
2017-04-22 13:50:55 +0200

    Added EntryInfoView and an Interface to go with it

---
+++ ROSViewsCDBG/ROS.Services/Models/EntryInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ROSPersistence.ROSDB;
+
+namespace ROS.Services.Models
+{
+    public class EntryInfo : IEntryInfo
+    {
+
+        private readonly Entry _entry;
+
+        public EntryInfo(Entry entry)
+        {
+          _entry = entry;
+        }
+        public int No { get { return _entry.No; } }
+
+        public int SkipperId { get { return _entry.SkipperId; } }
+
+        public string Description { get { return _entry.Description; } }
+
+       
+    }
+}
---
+++ ROSViewsCDBG/ROS.Services/Models/IEntryInfo.cs
@@ -0,0 +1,16 @@
+﻿using ROSPersistence.ROSDB;
+using System.Collections.Generic;
+
+namespace ROS.Services.Models
+{
+    public interface IEntryInfo
+    {      
+        int No { get; }
+        int SkipperId { get; }
+        IEnumerable<string> Boat{ get; }
+        IEnumerable<int>  Regatta { get; }
+        decimal TotalSumPaid { get; }
+        string Description { get; }
+
+    }
+}
