2cb2858e06a0d83d6a011b9cd72d5e404d714c57
danieljaller <danieljaller@gmail.com>
2017-04-23 00:26:17 +0200

    Revert "Fixed some small stuffs"
    This reverts commit 8e3ca5b8113402a9cc44d732eb0e3ac725452377.
    
    # Conflicts:
    #	ROSViewsCDBG/ROSViewsCDBG/ViewModels/EntryInfoViewModel.cs

--- ROSViewsCDBG/ROSViewsCDBG/ViewModels/EntryInfoViewModel.cs
+++ ROSViewsCDBG/ROSViewsCDBG/ViewModels/EntryInfoViewModel.cs
@@ -8,5 +8,20 @@ namespace ROSViewsCDBG.ViewModels
 {
     public class EntryInfoViewModel : ViewModelBase
     {
+        private string _boatName;
+        private int _regattaId;
+        private int _totalSumPaid;
+        private string _description;
+        private string _regattaName;
+        private ObservableCollection<string> _registeredUsers;
+                        
     }
 }
+        public string BoatName
+        public int RegattaId
+        public string RegattaName
+        public ObservableCollection<string> RegisteredUsers
+            set { _registeredUsers = value;OnPropertyChanged(); }
+            
+        }
+    }
