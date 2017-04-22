3874f6c531857b25431c5a9970510b6c1ca76822
danieljaller <danieljaller@gmail.com>
2017-04-23 00:44:05 +0200

    Revert "blablabla"
    This reverts commit 3c2d14f5d5a229614a1510c0ef5eec375d3d49dc.

--- ROSViewsCDBG/ROSViewsCDBG/ViewModels/EntryInfoViewModel.cs
+++ ROSViewsCDBG/ROSViewsCDBG/ViewModels/EntryInfoViewModel.cs
@@ -8,54 +8,62 @@ namespace ROSViewsCDBG.ViewModels
 {
     public class EntryInfoViewModel : ViewModelBase
     {
-        private string _boatName;
-        private int _regattaId;
         private int _no;
+        private int _skipperId;
         private int _totalSumPaid;
+        private string _description;
         private string _regattaName;
-        private ObservableCollection<string> _registeredUsers;
+        private ObservableCollection<RegisteredUser> _registeredUsers;
         private readonly IEntryService _entryService;
 
         public EntryInfoViewModel()
         {
             _entryService = ServiceLocator.Instance.EntryService;
-                        
-    	}
+
+        }
 
         public int No
         {
             get => _no;
             set { _no = value; OnPropertyChanged(); }
-    	}
-        public string BoatName
+        }
+        public int SkipperId
+        {
+            get => _skipperId;
+            set { _skipperId = value; OnPropertyChanged(); }
+        }
+        public Boat BoatName
         {
             get => _boatName;
-            set {_boatName = value; OnPropertyChanged(); }
+            set { _boatName = value; OnPropertyChanged(); }
         }
-        public int RegattaId
+        public Regatta RegattaId
         {
         get => _regattaId;
         set {_regattaId = value; OnPropertyChanged(); }
         }
-        public string RegattaName
-        {
-        get => _regattaName;
-        set {_regattaName = value; OnPropertyChanged(); }
-        }
-        public ObservableCollection<string> RegisteredUsers
-        {
-        get => _registeredUsers;
-        set {_registeredUsers = value; OnPropertyChanged(); }
-        }
-
         public int TotalSumPaid
         {
             get => _totalSumPaid;
             set { _totalSumPaid = value; OnPropertyChanged(); }
         }
-        
+        public string Description
+        {
+            get => _description;
+            set { _description = value; OnPropertyChanged(); }
+        }
+        public Regatta RegattaName
+        {
+        get => _regattaName;
+        set {_regattaName = value; OnPropertyChanged(); }
+        }
+        public ObservableCollection<RegisteredUser> RegisteredUsers
+        {
+        get => _registeredUsers;
+        set {_registeredUsers = value; OnPropertyChanged(); }
+            
 
+        }
     }
 }
-
 
