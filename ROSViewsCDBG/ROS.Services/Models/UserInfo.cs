89dc6fde858398258b03a85e5d22a91fb6f7e80b
danieljaller <danieljaller@gmail.com>
2017-04-22 11:21:31 +0200

    Switched around some stuff

--- ROSViewsCDBG/ROS.Services/Models/UserInfo.cs
+++ ROSViewsCDBG/ROS.Services/Models/UserInfo.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ROSPersistence.ROSDB;
+
 
 namespace ROS.Services.Models
 {
-    class UserInfoRecord
+    class UserInfo
     {
+        private readonly IUser _user;
+
+        public UserInfo(IUser user)
+        {
+            _user = user;
+        }
+
+        public string Name => $"{_user.FirstName} {_user.LastName}";
+        public IEnumerable<string> Club => _user.Members.SelectMany(u => u.Clubs.Select(c => c.Name));
+        public IEnumerable<UserContactInformation> ContactInformation => _user.UserContactInformations;
+
     }
 }
