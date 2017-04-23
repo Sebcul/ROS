4737081dfb4d46df8926088206b19ba19588bfef
Sebastian Cullbrand <cullbrand@gmail.com>
2017-04-23 11:32:14 +0200

    Added test

--- ROSViewsCDBG/ROS.Presentation.Tests/LoginTests/LoginUnitTests.cs
+++ ROSViewsCDBG/ROS.Presentation.Tests/LoginTests/LoginUnitTests.cs
@@ -4,11 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ROSViewsCDBG.ViewModels;
+using Moq;
 
 namespace ROS.Presentation.Tests.LoginTests
 {
     [TestClass]
     public class LoginUnitTests
     {
+        public void Should_ReturnString_When_EnteringEmail()
+        {
+            var email = "kalles.kaviar@abba.se";
+            var loginViewModel = new LoginWindowViewModel();
+
+            var result = loginViewModel.Email = email;
+
+            Assert.AreEqual(result, email);
+        }
     }
 }
